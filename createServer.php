f530a478255b18bfc253952676b45a53373fa541
Unbl0xed <160192405+Unbl0xed@users.noreply.github.com>
2024-04-01 20:45:29 -0500

    mcshit

---
+++ createServer.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+Client <?php echo $_POST["client"]; ?><br>
+IP <?php echo $_POST["ip"]; ?><br>
+Port <?php echo $_POST["port"]; ?><br>
+URI link <?php echo $_POST["uri"]; ?><br>
+Title <?php echo $_POST["title"]; ?>
+
+</body>
+</html>
